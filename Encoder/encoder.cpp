d734532558b43bdae49e68d3e1601e0173adf089
Jake Sakamoto <69215298+Jtsaka@users.noreply.github.com>
2025-09-04 22:57:07 -0700

    Update encoder.cpp

--- Encoder/encoder.cpp
+++ Encoder/encoder.cpp
@@ -1,70 +1,65 @@
 #include <Arduino.h> 
 #include "encoder.h"
+#include "spark.h"
 
-Encoder::Encoder(int pin, contServo servo) : encoderPin(pin), wiredServo(servo), currentAngle(0.0), targetAngle(0.0), tolerance(3.0), maxRange(2000), defaultSpd(1500), servoSpeed(defaultSpd), error(0.0), previousError(0.0), kp(3.0), ki(0.3), kd(1.5), integral(0.0){
+
+Encoder::Encoder(int pin, contServo& servo, Spark& spark) : encoderPin(pin), wiredSpark(spark), wiredServo(servo), currentAngle(0.0), targetAngle(0.0), tolerance(5.0), maxRange(300), defaultSpd(1500), servoSpeed(defaultSpd), error(0.0){
   pinMode(encoderPin, INPUT);
+  //targetAngle = readAngle();
+
 }
 
 void Encoder::goToDefaultPos(){ //set angle to 0, call in main
-  setTargetAngle(0.0);
-  setPID(getError());
+  setTargetAngle(0);
+  return;
 }
 
-void Encoder::setTargetAngle(float angle){ //call in main
-  targetAngle = angle;
-}
+void Encoder::setTargetAngle(float angle){ 
+  if (targetAngle != angle) {
+        targetAngle = angle;
+  }
+  return; 
+} 
 
-int Encoder::readAngle(){
-  float rawValue = analogRead(encoderPin);
+float Encoder::readAngle(){
+  int rawValue = analogRead(encoderPin);
   currentAngle = (rawValue / 1023.0) * 360.0; 
   return currentAngle;
 }
 
-float Encoder::closestAngle(float a, float b) {      //Calculate circular error 
+float Encoder::closestAngle(float target, float currentAngle) {      //Calculate circular error 
+  float errorCalc = fmod(target - currentAngle + 360.0, 360.0);      //error is positive
 
-  float errorCalc = fmod(b - a + 360.0, 360.0);      //error is positive
+  //maybe do a line of code where if errorCalc is 360, then make it 0? Only if oscillation occurs at 360-0
   if (errorCalc > 180) {
-      errorCalc -= 360;                              //Correct direction - counterclockwise
+      errorCalc -= 180;                              //Correct direction if greater than 180, counterclockwise
   }
   return errorCalc;
 }
 
-float Encoder::getError(){ //call in main within setPID
+float Encoder::goToAngle(){
   error = closestAngle(targetAngle, readAngle());
-  return error;
-}
+  if (abs(error) <= tolerance) {
+    //Serial.println("Stopped"); //prints infinite stops currently
+    wiredServo.stop();
+    return;
+  }
+  float speedFactor = abs(error) / 180.0 * maxRange;
 
-float Encoder::setPID(float error){ //call in main
-float pVal = kp * error;
-
-integral += error;                                //Track cumulative error
-float iVal = ki * integral;
-
-float dVal = kd * (error - previousError);
-previousError = error;
-
-float pidVal = pVal + iVal + dVal;
-
-if (abs(error) <= tolerance) {                      //Stop servo if within toleranced angle
-      servoSpeed = defaultSpd;                      //Stop the servo
-      integral = 0;                                 //Reset cumulataive error
-      pidVal = 0;                                   //Reset pid
+  // if(targetAngle >= 0 && targetAngle < 180){
+  //   wiredSpark.setSpeed(1800);
+  // }
+  // else{
+  //   wiredSpark.setSpeed(1200);
+  // }
+  
+  if (error > tolerance) {
+    servoSpeed = defaultSpd + constrain(speedFactor, 0, maxRange);
   } 
-else {
-    
-    if (pidVal > 0) {                             //maxRange constrains the pidVal (prevents erratic values)
-        servoSpeed = defaultSpd + constrain(pidVal, 1000, maxRange); 
-    } 
-    else {
-        servoSpeed = defaultSpd - constrain(-pidVal, 1000, maxRange);
-    }
-}
+  else {
+    servoSpeed = defaultSpd - constrain(speedFactor, 0, maxRange);
+  }
+  wiredServo.setSpeed(servoSpeed);
 
-wiredServo.setSpeed(servoSpeed);     //servo speed
-return pidVal;
-
-}
-
-int Encoder::getServoSpeed(){
   return servoSpeed;
 }
