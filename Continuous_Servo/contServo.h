6da6a502930ad10fbab5a739b78d144e58abb9a2
Jake Sakamoto <69215298+Jtsaka@users.noreply.github.com>
2025-09-15 08:54:48 -0700

    Update contServo.h
    contServo now references the encoder object instead of the encoder referencing the servo

--- Continuous_Servo/contServo.h
+++ Continuous_Servo/contServo.h
@@ -2,18 +2,35 @@
 #define CONTSERVO_H
 
 #include <Servo.h>
+#include "encoder.h"
 
 class contServo { 
   private:
-    int servoPin; //signal pin for servo
+    int servoPin; 
     int defaultSpd;
+    int tolerance;
+    int servoSpd;
+    int rotateSpd;
+    int totalSpd;
+    float error;
+    float integral;
+    float previousError;
+    unsigned long previousTime;
+    int driveDirection;
+
     Servo servo;
+    Encoder& encoder;
+
   public:
-    contServo(int pin);
+    contServo(int pin, Encoder& encoder);
     void initialize();
-    int getDefaultSpd();
     void setSpeed(int speed);
     void stop();
+
+    void setZero();
+    void goToAngle(int angle);
+    int closestAngle(int a, int b);
+
 };
 
 #endif
